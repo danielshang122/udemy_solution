45b8edb82dbf96ed23fb3dae4bf27ad53fb9ccfe
danielshang122 <danielshang122@gmail.com>
2021-07-01 15:22:33 -0400

    Add overriding demo

---
+++ overridingDemo/Editor.java
@@ -0,0 +1,13 @@
+package overridingDemo;
+
+
+
+public class Editor extends Staff {
+	public void printUserType() {
+	  System.out.println("Editor");
+	}
+	
+	public void approveReview() {
+		System.out.println("Editor: approveReview");
+	}
+}
---
+++ overridingDemo/Review.java
@@ -0,0 +1,17 @@
+package overridingDemo;
+
+
+public class Review {
+	private String reviewText;
+	private boolean approved;
+	
+	public Review (String reviewText) {
+		this.reviewText = reviewText;
+	}
+	public boolean isApproved() {
+		return approved;
+	}
+	public void setApproved(boolean approved) {
+		this.approved = approved;
+	}
+}
